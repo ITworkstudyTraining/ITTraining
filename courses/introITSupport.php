fb97f56c02a0ff26a0e1110fa0c66ced357bb5ee
a-curovic <alen.curovic@outlook.com>
2025-02-28 18:05:27 -0500

    Initial creation of introITSupport

---
+++ courses/introITSupport.php
@@ -0,0 +1,28 @@
+<html lang = "en">
+<head>
+    <meta charset = "UTF-8">
+    <meta name = "viewport" content = "width =device_width, initial-scale=1.0">
+    <title> Introduction to IT Support </title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <header>
+        <h1>Welcome to Introduction to IT Support!</h1>
+        <nav>
+            <ul>
+                <li><a href="../index.html">Home</a></li>
+                <li><a href="../courses/courseMain.html">Courses</a></li>
+                <li><a href="../dashboard.html">Dashboard</a></li>
+                <li><a href="../faq.html">FAQ</a></li>
+                <li><a href="../contact.html">Contact</a></li>
+                <li><a href="../login.html">Login</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <section>
+        <h3>How to create Tickets</h3>
+    </section>
+    
+</body>
+</html>
